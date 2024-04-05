d5a3c664fbf76b5098bcd3bbb0195f749e28fc62
sebastian461 <sebastiano-461@hotmail.com>
2024-04-05 11:48:29 -0500

    Estructura HTML

--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-selector-page',
   templateUrl: './selector-page.component.html',
-  styles: ``
+  styles: ``,
 })
 export class SelectorPageComponent {
+  public myForm: FormGroup = this.fb.group({
+    region: ['', Validators.required],
+    country: ['', Validators.required],
+    borders: ['', Validators.required],
+  });
 
+  constructor(private fb: FormBuilder) {}
 }
